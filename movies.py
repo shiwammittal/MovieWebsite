50b60d0e45fbee93e1bbe348e6c6a5448432620c
shiwammittal <shiwammittal@gmail.com>
2017-03-18 18:55:12 -0400

    Create the website and add movies to it

--- movies.py
+++ movies.py
@@ -2,10 +2,10 @@ import webbrowser
 
 class Movie ():
     def __init__(self, movie_title, movie_summary, movie_poster_image, movie_trailer):
-        self.movie_title = movie_title
-        self.movie_summary = movie_summary
-        self.movie_poster_image = movie_poster_image
-        self.movie_trailer = movie_trailer
+        self.title = movie_title
+        self.storyline = movie_summary
+        self.poster_image_url = movie_poster_image
+        self.trailer_youtube_url = movie_trailer
         
     def play_movie_trailer(self):
         webbrowser.open(self.movie_trailer)
