1b84863fa5aa3a14ae86aae0351c1a464d1a6e86
shiwammittal <shiwammittal@gmail.com>
2017-03-18 17:02:34 -0400

    Defining init
    Defining the features that will be displayed for each movie

---
+++ entertainment_center.py
@@ -0,0 +1,4 @@
+import movies
+
+toy_story = movies.Movie("Baahubali: The Beginning", "An ancient India, an adventurous and daring man becomes involved in a decades old feud between two warring people.", "http://www.imdb.com/title/tt2631186/mediaviewer/rm1394795264", "http://www.imdb.com/title/tt2631186/videoplayer/vi3834163993?ref_=tt_ov_vi")
+print(toy_story.movie_summary)
--- movies.py
+++ movies.py
@@ -1,1 +1,7 @@
-Class Movie():
+class Movie ():
+    def __init__(self, movie_title, movie_summary, movie_poster_image, movie_trailer):
+        self.movie_title = movie_title
+        self.movie_summary = movie_summary
+        self.movie_poster_image = movie_poster_image
+        self.movie_trailer = movie_trailer
+        
