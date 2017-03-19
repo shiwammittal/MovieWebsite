fcce2c727f2bec88d215958e32cbcdf285d00d3d
shiwammittal <shiwammittal@gmail.com>
2017-03-19 00:18:45 -0400

    Update Ratings

--- movies.py
+++ movies.py
@@ -1,6 +1,8 @@
 import webbrowser
 
 class Movie ():
+
+    VALID_RATINGS = ["G","P","PG-13","R"]
     def __init__(self, movie_title, movie_summary, movie_poster_image, movie_trailer):
         self.title = movie_title
         self.storyline = movie_summary
